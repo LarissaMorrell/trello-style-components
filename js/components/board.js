7f3e7e49218d6ef0b66f0b4f7077a1f00b30b5ca
LarissaMorrell <larissa@larissamorrell.com>
2017-03-19 10:57:08 -0400

    fixed syntax error in List.js => stupid function keyword. Now error of element type invalid

--- js/components/board.js
+++ js/components/board.js
@@ -4,14 +4,13 @@ import List from './list';
 export default function Board(props) {
     let lists = [];
     let cards = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "Vivamus at molestie turpis. Nunc quam erat, commodo eu gravida nec.", "Ut et dapibus mi. Aliquam et erat nec lorem tempor pretium."];
-    let listTitles = props.lists
+    let listTitles = props.lists;
     for (let i = 0; i < cards.length; i++) {
         let titleStr = "This is list title " + (i + 1);
         lists.push( 
             <List title={listTitles[i]}
                   cards={cards}/> );
-    }
-    console.log(lists);
+    };
 
     return ( 
         <div className="board">
--- js/components/list.js
+++ js/components/list.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM  from 'react-dom';
 import Card from './card';
 
 function Form(props){
@@ -17,11 +18,11 @@ class List extends React.Component{
         this.onAddInputChange = this.onAddInputChange.bind(this);
     }
     
-	function onAddInputChange(){
+	onAddInputChange(){
 		console.log("onAddInputChange is working");
 	}
 
-	function onAddSubmit(){
+	onAddSubmit(){
 		console.log("onAddSubmit is working");
 	}
 
@@ -33,7 +34,7 @@ class List extends React.Component{
 	              return <Card text={cardText} />})
 	          }
 	        </div>    //this.onAddInputChange refers to line 23 function
-	        <Form onAddInputChange={this.onAddInputChange} onAddSubmit={event.preventDefault(); this.onAddSubmit} />
+	        <Form onAddInputChange={this.onAddInputChange} onAddSubmit={() => {event.preventDefault(); this.onAddSubmit;}} />
 	      </div>
   	    );	
     }
